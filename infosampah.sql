40f24458c0a7bf863e5d070af6684d331c9bf10e
athoil <athoil286@gmail.com>
2016-02-04 15:39:56 +0700

    database update

--- infosampah.sql
+++ infosampah.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2016 at 12:57 PM
--- Server version: 5.5.46-0ubuntu0.14.04.2
+-- Generation Time: Feb 04, 2016 at 03:18 PM
+-- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `PantauSampah`
+-- Database: `infosampah`
 --
 
 -- --------------------------------------------------------
@@ -31,7 +31,8 @@ CREATE TABLE IF NOT EXISTS `data_sampah_user` (
   `id_user` int(11) NOT NULL,
   `input_sampah` double NOT NULL,
   `input_sampah_perbulan` double NOT NULL,
-  `action` enum('bakar','kirimtps','recycle','lain-lain') NOT NULL,
+  `action` int(3) NOT NULL,
+  `jenis_sampah` int(3) NOT NULL,
   `tanggal` datetime NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `edit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -57,6 +58,55 @@ CREATE TABLE IF NOT EXISTS `data_tips` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jenis_sampah`
+--
+
+CREATE TABLE IF NOT EXISTS `jenis_sampah` (
+  `id` int(2) NOT NULL,
+  `jenis sampah` varchar(50) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `edit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jenis_sampah`
+--
+
+INSERT INTO `jenis_sampah` (`id`, `jenis sampah`, `create_at`, `edit_at`) VALUES
+(1, 'Plastik', '2016-02-02 10:09:43', '0000-00-00 00:00:00'),
+(2, 'Kertas', '2016-02-02 10:09:43', '0000-00-00 00:00:00'),
+(3, 'Kaca', '2016-02-02 10:09:43', '0000-00-00 00:00:00'),
+(4, 'Logam', '2016-02-02 10:09:43', '0000-00-00 00:00:00'),
+(5, 'Sisa Makanan', '2016-02-02 10:09:43', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembuangan_sampah`
+--
+
+CREATE TABLE IF NOT EXISTS `pembuangan_sampah` (
+  `id` int(11) NOT NULL,
+  `pembuangan` varchar(50) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `edit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembuangan_sampah`
+--
+
+INSERT INTO `pembuangan_sampah` (`id`, `pembuangan`, `create_at`, `edit_at`) VALUES
+(1, 'dibakar', '2016-02-02 10:06:09', '0000-00-00 00:00:00'),
+(2, 'dikelola tps', '2016-02-02 10:06:09', '0000-00-00 00:00:00'),
+(3, 'dibuang', '2016-02-02 10:06:09', '0000-00-00 00:00:00'),
+(4, 'lain - lain', '2016-02-02 10:06:09', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -65,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `nama` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `role_id` enum('1','2') NOT NULL,
+  `role_id` int(2) NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `edit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
@@ -76,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `role_id`, `create_at`, `edit_at`) VALUES
-(1, 'admin', 'admin', 'popo', '1', '2016-02-01 11:37:09', '0000-00-00 00:00:00');
+(1, 'admin', 'admin', 'popo', 1, '2016-02-01 11:37:09', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
