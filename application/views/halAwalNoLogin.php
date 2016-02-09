1c461353424d8005029318ae4305ed6c1102c747
pbrilianis <patriciabriliani@gmail.com>
2016-02-09 18:11:53 +0700

    carousel

--- application/views/halAwalNoLogin.php
+++ application/views/halAwalNoLogin.php
@@ -1,16 +1,15 @@
 <?php $this->load->view('navbar') ?>
-<body>
-    <!--carousel-->
-      <div id="myCarousel" class="carousel slide">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
 
+  <!--carousel
+    <div id="MyCarousel" class="carousel slide">
+        <ol class="carousel-indicators">
+          <li data-target ="#MyCarousel" data-slide-to ="0" class="active"></li>
+          <li data-target ="#MyCarousel" data-slide-to ="1"></li>
+          <li data-target ="#MyCarousel" data-slide-to ="2"></li>
+        </ol>
         <div class="carousel-inner">
           <div class="item active">
-            <img src="../../asset/img/floor.jpg" alt="floor" class="img-responsive" />
+            <img src="../../asset/img/floor.jpg" alt="floor" class="img-responsive">
             <div class="carousel-caption">
               <h3>FLOOR</h3>
               <p>
@@ -23,12 +22,11 @@
               </p>
             </div>
           </div>
-
           <div class="item">
-            <img src="../../asset/img/city.jpg" alt="floor" class="img-responsive" />
-            <div class="carousel-caption">
-              <h3>CITY</h3>
-              <p>
+            <img src="../../asset/img/space.jpg" alt="floor" class="img-responsive">
+              <div class="carousel-caption">
+                <h3>SPACE</h3>
+                <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -36,38 +34,34 @@
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-            </div>
+              </div>
           </div>
-
           <div class="item">
-            <img src="../../asset/img/space.jpg" alt="floor" class="img-responsive" />
-            <div class="carousel-caption">
-              <h3>SPACE</h3>
-              <p>
+            <img src="../../asset/img/city.jpg" alt="floor" class="img-responsive">
+              <div class="carousel-caption">
+                <h3>City</h3>
+                <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
+                </p>
+              </div>
           </div>
         </div>
-
-        <a class="carousel-control left" data-slide="prev" href="myCarousel" >
+        <a class="carousel-control left" data-slide="prev" href="#MyCarousel">
           <span class="icon-prev"></span>
         </a>
-
-        <a class="carousel-control right" data-slide="next" href="myCarousel" >
+              <a class="carousel-control right" data-slide="next" href="#MyCarousel">
           <span class="icon-next"></span>
         </a>
-
-      </div>
+    </div>
     <!--ends of carousel-->
 
         <!--<script src="<?php echo base_url()?>asset/js/jquery.min.js"></script>-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="<?php echo base_url()?>asset/js/jquery.min.js"></script>
         <script src="<?php echo base_url()?>asset/js/bootstrap.min.js"></script>
   </body>
 </html>
