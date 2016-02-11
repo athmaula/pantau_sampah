920087b43f5ed62e6a6088965d056e169e79a27d
pbrilianis <patriciabriliani@gmail.com>
2016-02-11 15:01:22 +0700

    ini footer tok, belum di warnai

---
+++ application/views/footer.php
@@ -0,0 +1,32 @@
+<?php $this->load->view('header') ?>
+<!--start of footer-->
+<div class="bottom">
+        <div class="container-fluid">
+            <div class="col-md-8 col-sm-4">
+                <h4>PantauSampah</h4>
+                <p>
+                Aplikasi ini dibuat dengan tujuan menginformasikan user seputar produksi sampah di Semarang, terutama sampah rumah
+                tangga dan dampaknya bila tidak di kelola dengan baik.
+                Selain itu aplikasi ini dapat mengitung jumlah sampah yang dihasilkan oleh user perhari dan menghitung jumlah sampah
+                yang di produksi warga kota Semarang setiap harinya sambil menampilkan fakta fakta unik seputar sampah.
+                </p>
+                <hr>
+            </div>
+            <div class="col-md-4 col-sm-4">
+                <h4><span class="glyphicon glyphicon-send">Kontak Kami</span></h4>
+
+                <p>pantausampah@gmail.com</p>
+                <p>athoil286@gmail.com</p>
+                <p>patriciabriliani@gmail.com</p>
+                <hr>
+            </div>
+        </div>
+    </div>
+<!--end of footer-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="<?php echo base_url()?>asset/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</body>
+</html>
