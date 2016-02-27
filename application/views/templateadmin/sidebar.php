a524a46869fdce926b849939a8afb1526565de10
pbrilianis <patriciabriliani@gmail.com>
2016-02-28 01:26:38 +0700

    edit bahasa dan tampilan

--- application/views/templateadmin/sidebar.php
+++ application/views/templateadmin/sidebar.php
@@ -1,25 +1,25 @@
 <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
+  <aside class="main-sidebar sidebar-background">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
       <!-- Sidebar user panel -->
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
-        <li><a href="<?php echo site_url('admin') ?>"><i class="fa fa-table"></i> Dashboard</a></li>
-        <li><a href="<?php echo site_url('crud') ?>"><i class="fa fa-table"></i> User Table</a></li>
+        <li class="header" style="color:#004d40;">MAIN NAVIGATION</li>
+        <li><a href="<?php echo site_url('admin') ?>" style="color:#00695c;"><i class="fa fa-table"></i> Dashboard</a></li>
+        <li><a href="<?php echo site_url('crud') ?>" style="color:#00695c;"><i class="fa fa-table"></i> User Table</a></li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-info"></i>
-            <span>TIPS</span>
+            <i class="fa fa-info" style="color:#00695c;"></i>
+            <span style="color:#00695c;">Tips</span>
             <span class="label label-primary pull-right"></span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('admin/view_input_tips');?>"><i class="fa fa-pencil"></i> Input Tips LandingPage User</a></li>
-            <li><a href="<?php echo site_url('admin/view_input_tips_homepage');?>"><i class="fa fa-pencil"></i> Input Tips HomePage</a></li>
+            <li><a href="<?php echo site_url('admin/view_input_tips');?>" style="color:#00695c;"><i class="fa fa-pencil"></i> Input Tips in LandingPage User</a></li>
+            <li><a href="<?php echo site_url('admin/view_input_tips_homepage');?>" style="color:#00695c;"><i class="fa fa-pencil"></i> Input Tips in HomePage</a></li>
          </ul>
-        </li> 
+        </li>
       </ul>
     </section>
     <!-- /.sidebar -->
